0a5c66e0e7fccac705bc2e701189097dfa8badf2
JoseIgnacioTapia <josignaciot@gmail.com>
2021-09-24 16:56:40 -0300

    implementing portals for the modal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,23 +1,45 @@
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 import classes from './ErrorModal.module.css';
 import Card from './Card';
 import Button from './Button';
 
+const Backdrop = props => {
+  return <div className={classes.backdrop} onClick={props.onCloseModal} />;
+};
+
+const ModalOverlay = props => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.content}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onCloseModal}>Ok!</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = props => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onCloseModal} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.content}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onCloseModal}>Ok!</Button>
-        </footer>
-      </Card>
-    </div>
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onCloseModal={props.onCloseModal} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          content={props.content}
+          onCloseModal={props.onCloseModal}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </Fragment>
   );
 };
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
@@ -42,7 +42,7 @@ const AddUser = props => {
   };
 
   return (
-    <div>
+    <Fragment>
       {error && (
         <ErrorModal
           title={error.title}
@@ -69,7 +69,7 @@ const AddUser = props => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Fragment>
   );
 };
 
